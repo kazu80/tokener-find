769027448891f1f1dc921795891f96c988dd3eeb
kawakami <saruyama.monki@gmail.com>
2021-01-25 16:03:06 +0900

    webpack + scss + file copy

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const CopyWebpackPlugin       = require('copy-webpack-plugin');
+
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
     mode: 'development',
 
-    // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: './src/assets/ts/main.ts',
+    entry: {
+        index: [
+            './src/assets/ts/main.ts',
+            './src/assets/scss/main.scss'
+        ],
+    },
 
     // ファイルの出力設定
     output: {
@@ -29,8 +36,17 @@ module.exports = {
                 // TypeScript をコンパイルする
                 use: 'ts-loader',
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'},
+                ],
+            }
         ],
     },
+
     // import 文で .ts ファイルを解決するため
     // これを定義しないと import 文で拡張子を書く必要が生まれる。
     // フロントエンドの開発では拡張子を省略することが多いので、
@@ -41,4 +57,35 @@ module.exports = {
             '.ts', '.js',
         ],
     },
+
+    plugins: [
+        // cssの出力先を指定する
+        new MiniCssExtractPlugin({filename: '../css/[name].css'}),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './',
+                    to: '../../',
+                    filter: async (resourcePath) => {
+                        return new RegExp('\.html$').test(resourcePath);
+                    },
+                    context: 'src'
+                },
+            ],
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './',
+                    to: '../images/',
+                    filter: async (resourcePath) => {
+                        return new RegExp('\.(png|jpg|jpeg|gif|svg)$').test(resourcePath);
+                    },
+                    context: 'src/assets/images'
+                },
+            ]
+        })
+    ]
 };
