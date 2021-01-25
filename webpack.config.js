cbbafdd578e52bd1288a76e2d18bdb8424dedc7e
kawakami <saruyama.monki@gmail.com>
2021-01-25 14:12:42 +0900

    webpack + typescript

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: './src/assets/ts/main.ts',
+
+    // ファイルの出力設定
+    output: {
+        //  出力ファイルのディレクトリ名
+        path: `${__dirname}/dist/assets/js`,
+        // 出力ファイル名
+        filename: "main.js"
+    },
+
+    // ローカル開発用環境を立ち上げる
+    // 実行時にブラウザが自動的に localhost を開く
+    devServer: {
+        contentBase: "dist",
+        open: true
+    },
+
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader',
+            },
+        ],
+    },
+    // import 文で .ts ファイルを解決するため
+    // これを定義しないと import 文で拡張子を書く必要が生まれる。
+    // フロントエンドの開発では拡張子を省略することが多いので、
+    // 記載したほうがトラブルに巻き込まれにくい。
+    resolve: {
+        // 拡張子を配列で指定
+        extensions: [
+            '.ts', '.js',
+        ],
+    },
+};
