2860c48d89c9a1e1b6a5728b41fff0f9dc95bd79
kawakami <saruyama.monki@gmail.com>
2021-02-16 21:08:05 +0900

    sample coding

--- src/assets/ts/main.ts
+++
@@ -1,6 +0,0 @@
-import { hello } from "./sub";
-
-const message: string = "Hello World";
-
-// sub.jsに定義されたJavaScriptを実行する。
-hello(message);
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     // ローカル開発用環境を立ち上げる
     // 実行時にブラウザが自動的に localhost を開く
     devServer: {
-        contentBase: "./dist",
+        contentBase: "dist",
         port: 3000,
         open: true
     },
